302d04c0dc871a6e1c50433f4727506a8828b528
ezio <tirimaxx@gmail.com>
2024-07-30 19:26:13 +0200

    Implementazione iniziale header-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from "./footer-component/footer-component.component";
+import { GridComponentComponent } from "./grid-component/grid-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponentComponent, FooterComponentComponent, GridComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  //template: `<app-header-component [header]="headerTitle"></app-header-component>`
 })
 export class AppComponent {
-  title = 'Creazione componente custom';
+
+  headerTitle: string = 'Creazione componenti angular';
+  scope: string = 'Test pratico 2';
+
 }
